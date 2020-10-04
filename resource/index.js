1645f86e6dbfac1ee0af64ba8144b612301a1a1d
Shaan Subbaiah <shaansubbaiah@yahoo.com>
2020-10-04 14:10:55 +0530

    Fix repo cards overlap
    Masonry is now called in index.js and reloads the layout on page load.

--- resource/index.js
+++ resource/index.js
@@ -1,6 +1,5 @@
-//              Dark mode toggle
+//          Dark mode toggle
 // @SEE https://ryanfeigenbaum.com/dark-mode/
-//
 if (window.CSS && CSS.supports("color", "var(--primary)")) {
   var toggleColorMode = function toggleColorMode(e) {
     // Switch to Light Mode
@@ -30,3 +29,18 @@ if (window.CSS && CSS.supports("color", "var(--primary)")) {
   var btnContainer = document.querySelector(".color-mode-toggle");
   btnContainer.style.display = "none";
 }
+
+//          Masonry for the repository cards
+// @SEE https://masonry.desandro.com/
+let msnry = new Masonry(".grid", {
+  itemSelector: ".grid-item",
+  gutter: 20,
+  horizontalOrder: true,
+  columnWidth: 350,
+  fitWidth: true,
+});
+
+// Force masonry reload once page is ready to prevent overlap
+window.onload = function () {
+  msnry.layout();
+};
