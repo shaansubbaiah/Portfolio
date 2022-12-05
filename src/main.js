de0af18d3dc541f15dde9e2d91ae5676226f4e1b
Shaan Subbaiah <shaansubbaiah@yahoo.com>
2022-12-05 14:06:58 +0530

    Serve masonry locally

--- src/main.js
+++ src/main.js
@@ -2,7 +2,10 @@ import "@fontsource/poppins/400.css";
 import "@fontsource/poppins/500.css";
 import "@fontsource/poppins/600.css";
 import "@fontsource/poppins/700.css";
+
 import "./style.css";
+
+import Masonry from "masonry-layout";
 
 //          Dark mode toggle
 // @SEE https://ryanfeigenbaum.com/dark-mode/
